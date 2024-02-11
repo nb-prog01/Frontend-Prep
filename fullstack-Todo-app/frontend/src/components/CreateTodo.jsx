aff58f14ac10960914f2cfdffc2ab0e073193e54
nb-prog01 <nirajbhadoria007@gmail.com>
2024-02-12 00:42:05 +0530

    Fullstack todo completed <deploy and some fixes pending>

--- fullstack-Todo-app/frontend/src/components/CreateTodo.jsx
+++ fullstack-Todo-app/frontend/src/components/CreateTodo.jsx
@@ -1,15 +1,37 @@
+
+import { useState } from "react";
+
 export function CreateTodo(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+//// onChange is causing infinte re-rendering which needs to be fixed
+
     return <div>
-        <input type="text" placeholder="title"></input><br/>
-        <input type="text" placeholder="description"></input><br/>
+        <input type="text" placeholder="title" onChange={function(e) {
+            const value = e.target.value;
+            setTitle(e.target.value);
+        }}></input><br/>
+        <input type="text" placeholder="description" onChange={function(e) {
+            const value = e.target.value;
+            setDescription(e.target.value);
+        }}></input><br/>
 
         <button onClick={()=>{
-            fetch("http://localhost:3000/todo",{
-                method: "POST"
+            // axios
+            fetch("http://localhost:3000/todo", {
+                method: "POST",
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
             })
             .then(async function(res){
-              const json=await res.json;
-              alert("Todo created")
+                const json=await res.json;
+                alert("Todo created")
             })
         }}>Add a ToDo</button>
     </div>
