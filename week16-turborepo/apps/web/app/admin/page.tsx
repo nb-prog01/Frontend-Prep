d09e16e5b2f9bd85ebab976a9392ae0e3147ba1b
nb-prog01 <nirajbhadoria007@gmail.com>
2024-05-27 00:34:43 +0530

    turborepo class

---
+++ week16-turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function(){
+    return <div>
+        hi from admin 
+        <Admin/>
+    </div>
+}
---
+++ week16-turborepo/packages/ui/src/newcomp.tsx
@@ -0,0 +1,8 @@
+export const Newcomp = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <h1>Newcomp Component</h1>
+      {children}
+    </div>
+  );
+};
