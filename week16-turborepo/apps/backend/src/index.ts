4af78b9d9d16d1d9421636001270d39a2b3a3ca7
nb-prog01 <nirajbhadoria007@gmail.com>
2024-05-27 01:35:48 +0530

    turborepo

--- week16-turborepo/apps/backend/src/index.ts
+++ week16-turborepo/apps/backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
-
+import { BACKEND_URL } from "@repo/common/config"
 const app=express();
+
+console.log(BACKEND_URL)
 
 app.get("/" , (req,res)=>{
     res.json({
---
+++ week16-turborepo/packages/common/src/index.ts
@@ -0,0 +1,2 @@
+export const VALUE="niraj bhadoria";
+export const BACKEND_URL="https://api.google.com"
